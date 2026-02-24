1abb5788a174770757b31f5f3a77ac1d4dcc21d6
WizzDom <jeddachppc@gmail.com>
2026-02-24 10:13:55 +0000

    Update personal.js

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
-const Age = () => {
-  const [age, setAge] = useState();
+// const Age = () => {
+//   const [age, setAge] = useState();
 
-  const tick = () => {
-    const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('2003-10-28T09:24:00');
-    setAge(((Date.now() - birthTime) / divisor).toFixed(11));
-  };
+//   const tick = () => {
+//     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
+//     const birthTime = new Date('2003-10-28T09:24:00');
+//     setAge(((Date.now() - birthTime) / divisor).toFixed(11));
+//   };
 
-  useEffect(() => {
-    const timer = setInterval(() => tick(), 21);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-  return <>{age}</>;
-};
+//   useEffect(() => {
+//     const timer = setInterval(() => tick(), 21);
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, []);
+//   return <>{age}</>;
+// };
 
 const data = [
-  {
-    key: 'age',
-    label: 'Current age',
-    value: <Age />,
-  },
+  // {
+  //   key: 'age',
+  //   label: 'Current age',
+  //   value: <Age />,
+  // },
   {
     key: 'countries',
     label: 'Countries visited',
@@ -32,7 +32,7 @@ const data = [
   {
     key: 'location',
     label: 'Current city',
-    value: 'Leicester, England',
+    value: 'London, England',
   },
 ];
 
